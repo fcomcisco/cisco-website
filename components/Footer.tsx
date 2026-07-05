ad29af137229d953139af1d42bc37f27ddde11d9
fcomcisco <francisco.montecinos1999@gmail.com>
2026-07-05 13:18:14 -0400

    Replace Next.js app with single-file static portfolio site

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="fixed bottom-0 left-0 right-0 bg-black/70 text-white text-center py-4 z-50">
-      <p className="text-sm">
-        &copy; {new Date().getFullYear()} My Portfolio. All rights reserved.
-      </p>
-    </footer>
-  );
-}
